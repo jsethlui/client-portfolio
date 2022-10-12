f510cb47217b3d1cffd9c30b57d9b277532f9d51
Jeremy Louie <jeremysf74@yahoo.com>
2022-10-12 12:09:46 -0700

    Node communicates with PostgreSQL

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,35 @@
 
 const express = require("express");
 const cors = require("cors");
+const knex = require("knex");
+require('dotenv').config();
+
+const database = knex({
+    client: "pg",
+    connection: {
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE,
+    },
+});
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.user(cors());
-app.get("/", (req, res) => res.send("Home Route"));
-const port = process.env.PORT || 5000;
+app.use(cors());
+app.get("/", (req, res) => {
+    database.select("*")
+        .from("client")
+        .then((data) => {
+            console.log(data)
+            res.json(data)
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+});
+
+const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Server running on port ${port}, http://localhost:${port}`));
