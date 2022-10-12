870ab26fbba37a2ba40249017ae4ce752ccd85e5
Jeremy Louie <jeremysf74@yahoo.com>
2022-10-12 12:32:39 -0700

    Frontend communicates with backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,36 @@
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Home: React.FC = () => {
+    useEffect(() => {
+        const getAPI = () => {
+            const API = 'http://127.0.0.1:5001/';
+            fetch (API)
+                .then((response) => {
+                    console.log(response);
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    setLoading(false);
+                    setupApiData(data);
+                });
+        };
+        getAPI();
+    }, []);
+    const [apiData, setupApiData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     return (
-        <p>test</p>
+        <p>      
+            <section>
+                {apiData.map((client) => {
+                    return(
+                        <p>{client.first_name}</p>
+                    )
+                })}
+            </section>    
+        </p>
     );
 };
 
