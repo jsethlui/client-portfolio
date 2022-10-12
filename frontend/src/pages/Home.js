d366e099bd0a0303289bac8dac748f1f5765c26c
Jeremy Louie <jeremysf74@yahoo.com>
2022-10-12 13:05:28 -0700

    Added some Header styling

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,38 +1,14 @@
 
 import React, { useState, useEffect } from 'react'
+import Header from "./components/Header.js"
 import "./Global.css"
 import "./Home.css"
 
 const Home: React.FC = () => {
-    useEffect(() => {
-        const getAPI = () => {
-            const API = 'http://127.0.0.1:5001/';
-            fetch (API)
-                .then((response) => {
-                    console.log(response);
-                    return response.json();
-                })
-                .then((data) => {
-                    console.log(data);
-                    setLoading(false);
-                    setupApiData(data);
-                });
-        };
-        getAPI();
-    }, []);
-    const [apiData, setupApiData] = useState([]);
-    const [loading, setLoading] = useState(true);
-
     return (
-        <p>      
-            <section>
-                {apiData.map((client) => {
-                    return(
-                        <p>{client.first_name}</p>
-                    )
-                })}
-            </section>    
-        </p>
+        <div className="home-body">
+            <Header />
+        </div>
     );
 };
 
