703887241c1bc7bf5eae24a133944aea0d36c96b
Jeremy Louie <jeremysf74@yahoo.com>
2022-10-12 20:04:56 -0700

    Minor changes to header

--- frontend/src/pages/components/Header.js
+++ frontend/src/pages/components/Header.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import "./Header.css"
 
 const Header: React.FC = () => {
+    // Source: https://andrewbaisden.medium.com/creating-react-node-apps-that-connect-to-postgresql-and-harperdb-2734771c5261
     useEffect(() => {
         const getAPI = () => {
             const API = 'http://127.0.0.1:5001/';
